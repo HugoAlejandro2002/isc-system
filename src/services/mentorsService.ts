ea9b07391a0fa6f74f4ac499e46705a4790bd628
Paul Landaeta <paulwilkerlf@gmail.com>
2024-01-09 19:25:34 -0400

    fix: use vercel url

--- src/services/mentorsService.ts
+++ src/services/mentorsService.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8000/api/';
+const API_URL = 'https://isc-system-backend.vercel.app/api/';
 
 const getMentors = async () => {
     try {

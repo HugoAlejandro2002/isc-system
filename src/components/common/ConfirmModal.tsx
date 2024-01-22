7f0a391c00dfd9816963b1e10d95057893c8d92b
Amy312 <katsukiamy1.gmail.com>
2024-01-21 22:56:32 -0400

    changed bg

--- src/components/common/ConfirmModal.tsx
+++ src/components/common/ConfirmModal.tsx
@@ -7,8 +7,7 @@ interface confirmModal {
 }
 const ConfirmModal = (props: confirmModal) => {
   return (
-    <div className="flex absolute items-center justify-center inset-0 bg-opacity-15 bg-slate-600">
-      <div className="flex flex-col justify-center bg-white m-5 p-5 shadow-md rounded-lg h-fit w-1/4">
+<div className="flex absolute  items-center justify-center inset-0  bg-gradient-to-tr from-[#39414E]/90 from-40% via-50%  to-[#39414E]/95 to-55% via-[#272F3C]/90  z-50 bg-opacity-55">      <div className="flex flex-col justify-center bg-white m-5 p-5 shadow-md rounded-lg h-fit w-1/4">
         <label className="txt-modal">Finalizando etapa: {props.step}</label>
         <label className="txt2-modal ">¿Está seguro de continuar a la siguiente etapa de {props.nextStep}?</label>
         <label className="txt3-modal">No podrá modificar los datos una vez que continue</label>
